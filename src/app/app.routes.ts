97df6f5527e0471cd78adcd70f6a73de87729d21
Fernanda <fernanda.souza@grupopan.com>
2024-10-27 23:19:42 -0300

    feature: adicionando componente de lista e rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './features/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'lista', component: ListComponent},
+    {path: '', redirectTo: 'lista', pathMatch: 'full'}
+];
