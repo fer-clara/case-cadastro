d43cd1758b20ba6f71dde46c2bccf0b87811884d
Fernanda <fernanda.souza@grupopan.com>
2024-10-27 22:36:36 -0300

    configurações iniciais e criação do header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+    <app-header />
+    <router-outlet />
+  `,
 })
-export class AppComponent {
-  title = 'case-cadastro';
-}
+export class AppComponent {}
