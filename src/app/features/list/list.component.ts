6f60375b5f04625ea6acbd39024b3cc27bacbe52
Fernanda <fernanda.souza@grupopan.com>
2024-10-31 17:49:13 -0300

    fix: removendo componente de card da lista

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,20 +2,16 @@ import { Component, inject, signal } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { ItemsService } from '../../shared/services/items.service';
 import { Item } from '../../shared/models/item.interface';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { CommonModule } from '@angular/common';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [HeaderComponent, MatCardModule, MatButtonModule, MatExpansionModule, CommonModule],
+  imports: [HeaderComponent, CardComponent],
   templateUrl: './list.component.html',  
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
-  readonly panelOpenState = signal(false);
   items: Item[] = [];
   httpClient = inject(ItemsService);
 
