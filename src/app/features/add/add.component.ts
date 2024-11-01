233eb5fc6020d3f3466d49c3c54a0ecca3e0abf8
Fernanda <fernanda.souza@grupopan.com>
2024-11-01 18:27:49 -0300

    feature: criando formulário dentro do componente add

--- src/app/features/add/add.component.ts
+++ src/app/features/add/add.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './add.component.html',
   styleUrl: './add.component.scss'
 })
 export class AddComponent {
+  form = new FormGroup({
+    nome: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    quantidade: new FormControl<number>(0, {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+  });
 
+  onSubmit() {
+    this.form.controls.nome.value;
+  }
 }
