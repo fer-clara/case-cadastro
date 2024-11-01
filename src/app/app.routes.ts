1172b47bb741be2585b67a9934c7e7ef2c7a19dd
Fernanda <fernanda.souza@grupopan.com>
2024-11-01 14:59:03 -0300

    feature: adicionando rota de cadastro de item

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
+import { AddComponent } from './features/add/add.component';
 
 export const routes: Routes = [
     {path: 'lista', component: ListComponent},
-    {path: '', redirectTo: 'lista', pathMatch: 'full'}
+    {path: '', redirectTo: 'lista', pathMatch: 'full'},
+    {path: 'cadastrar', component: AddComponent}
 ];
