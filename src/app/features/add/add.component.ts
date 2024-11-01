dba68958ad551fbfa104f24bc787fd45739aef57
Fernanda <fernanda.souza@grupopan.com>
2024-11-01 16:37:30 -0300

    feature: adicionando header ao card

--- src/app/features/add/add.component.ts
+++ src/app/features/add/add.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './add.component.html',
   styleUrl: './add.component.scss'
 })
