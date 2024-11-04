3ed612068bcd979ef0724d56ebefdeb236068bfa
Fernanda <fernanda.souza@grupopan.com>
2024-11-04 14:56:01 -0300

    feature: criando rota para edição do item

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
 import { AddComponent } from './features/add/add.component';
+import { EditComponent } from './features/edit/edit.component';
 
 export const routes: Routes = [
     {path: 'lista', component: ListComponent},
     {path: '', redirectTo: 'lista', pathMatch: 'full'},
-    {path: 'cadastrar', loadComponent: () => import('./features/add/add.component').then(m => AddComponent)}
+    {path: 'cadastrar', loadComponent: () => import('./features/add/add.component').then(m => AddComponent)},
+    {path: 'editar', loadComponent: () => import('./features/edit/edit.component').then(m => EditComponent)}
 ];
