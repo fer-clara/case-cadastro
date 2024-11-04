48a308cc8c32e6ac2e9641f040da6a18522d7dff
Fernanda <fernanda.souza@grupopan.com>
2024-11-04 12:01:19 -0300

    feature: adicionando serviço para adicionar item no submit

--- src/app/features/add/add.component.ts
+++ src/app/features/add/add.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field'; 
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { ItemsService } from '../../shared/services/items.service';
+import { unidadeDeMedida } from '../../shared/models/item.interface';
+import { MatSnackBar } from '@angular/material/snack-bar'
 
 
 @Component({
@@ -14,6 +17,9 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './add.component.scss'
 })
 export class AddComponent {
+  itemService = inject(ItemsService);
+  matSnackBar = inject(MatSnackBar);
+
   form = new FormGroup({
     nome: new FormControl<string>('', {
       nonNullable: true,
@@ -26,6 +32,14 @@ export class AddComponent {
   });
 
   onSubmit() {
-    this.form.controls.nome.value;
+    this.itemService.postItem({
+      nome: this.form.controls.nome.value,
+      unidadeDeMedida: unidadeDeMedida.kg,
+      quantidade: 1,
+      preco: 1,
+      perecivel: true
+    }).subscribe(() => {
+      alert('success')
+    })
   }
 }
